2cfc3a7c75a3e1bfba377aecd976894134766cbd
AidenSPS <aidenkusmierczak2016@gmail.com>
2023-02-06 20:55:09 -0500

    startStory and info work

--- story.js
+++ story.js
@@ -2,3 +2,14 @@
 //This will not have any DOM scripts either, just the transition functions. 
 //The HTMLObjects will be created through HTML, not JavaScript
 //However this will set the text of the main HTMLObject (paragraphs mainly) and will be assigned a function for each for a specific story order. 
+
+
+//Paragraph tags for storyX.html
+
+function startStory(){
+    document.location = 'story1.html';
+}
+
+function sourceInformation(){
+    document.location = "README.md";
+}
