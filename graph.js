65fb3adef4943d21c4a17d91141619b246b6d4b3
AidenSPS <aidenkusmierczak2016@gmail.com>
2023-02-06 23:59:36 -0500

    images in graph.html appear
    Resize? For presentation purpose yes

--- graph.js
+++ graph.js
@@ -1,15 +1,32 @@
-function duckGraph(){
+//DOM Elements 
+//ID's to the Images
+    var duckCalc = document.getElementById("duckCalc");
+    var thimbleCalc = document.getElementById("thimbleCalc");
+    var depositCalc = document.getElementById("depositCalc");
+    var tableCalc = document.getElementById("tablecalc");
 
+//Getting the Button ID's 
+    var duckButton = document.getElementById("duckButton");
+    var thimbleButton = document.getElementById("thimbleButton");
+    var depositButton = document.getElementById("depositButton");
+    var tableButton = document.getElementById("tableButton");
+
+function duckGraph(){
+    duckCalc.style.display = "block";
+    duckButton.style.display = "none";
 }
 
 function thimbleGraph(){
-
+    thimbleClac.style.display = "block";
+    thimbleButton.style.display = "none";
 }
 
 function depositGraph(){
-
+    depositCalc.style.display = "block";
+    depositButton.style.display = "none";
 }
 
 function depositTable(){
-    
+    depositCalc.style.display = "block";
+    depositButton.style.display = "none";
 }
